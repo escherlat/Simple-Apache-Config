c04521089cd3a512cbb566e5766b578f8bfd5f61
Kenneth Power <kpower@aezula.com>
2010-10-15 22:43:07 -0500

    Simple tidy and removed hard coded values

--- account_create.pl
+++ account_create.pl
@@ -11,15 +11,13 @@ use lib '/usr/local/cpanel';
 use Template;
 use Cpanel::Config::userdata::Load;
 
-my $template_dir = '/root/simple_config_template';
-my $user = 'kpowerme';
-my $domain = 'kpower.me';
+my ( $user, $domain, $template_dir  );
 
 my $userdata->{'vhost'} = Cpanel::Config::userdata::Load::load_userdata( $user, $domain );
 
 my $template = Template->new(
     INCLUDE_PATH => $template_dir,
-    );
+);
 
 $template->process( 'simple_template.tpl', $userdata ) || print Dumper $template->error();
 
