8b51211903ec4d8c62b028e28df4e51a935654e0
Kenneth Power <kpower@aezula.com>
2010-10-20 16:11:08 -0500

    Rearrange logic to obtain userdata late in the process, needed for adding Addon domains. Improved logging. Fetch userdata for subdomain and apply overrides, when adding an Addon domain.

--- lib/SimpleTemplate/Add.pm
+++ lib/SimpleTemplate/Add.pm
@@ -19,63 +19,78 @@ sub new{
 sub update_config{
     my ( $self ) = @_;
 
-    if( $self->_load_userdata() ){
-        my %vhost;
-        $vhost{'vhost'} = $self->{'vhost'};
-        if( $self->{'domain_type'} eq 'addon' ){
-            eval {
-                require SimpleTemplate::AddonDomain;
-            };
-            if( ! $@ ){
-                my $addon_domain = SimpleTemplate::AddonDomain::get_actual_domain( $self->{'user'}, $self->{'domain'} );
-                if( $addon_domain ){
-                    $vhost{'vhost'}{'servername'} = $addon_domain;
-                    $vhost{'vhost'}{'serveralias'} = 'www.'.$addon_domain;
-                    $vhost{'vhost'}{'serveradmin'} =~ s/$self->{'domain'}/$addon_domain/;
-                    $self->{'domain'} = $addon_domain;
+    my %vhost;
+    $vhost{'vhost'} = '';
+    if( $self->{'domain_type'} eq 'addon' ){
+        eval {
+            require SimpleTemplate::AddonDomain;
+        };
+        if( ! $@ ){
+            my $sub_domain = SimpleTemplate::AddonDomain::get_sub_domain( $self->{'user'}, $self->{'domain'} );
+            if( $sub_domain ){
+                if( $self->_load_userdata( $sub_domain ) ){
+                    $vhost{'vhost'} = $self->{'vhost'};
+                    $vhost{'vhost'}{'servername'} = $self->{'domain'};
+                    $vhost{'vhost'}{'serveralias'} = 'www.'.$self->{'domain'};
+                    $vhost{'vhost'}{'serveradmin'} =~ s/$sub_domain/$self->{'domain'}/;
+                }
+                else{
+                    Cpanel::Logger::logger(
+                        {
+                            'message' => 'Unable to load userdata for '. $sub_domain,
+                            'level' => 'warn',
+                            'service' => 'simpletemplate::add'
+                        }
+                    );
                 }
             }
             else{
                 Cpanel::Logger::logger(
                     {
-                        "message" => "Unable to load SimpleTemplate::AddonDomain: $@",
-                        "level" => "warn",
-                        "backtrace" => 0,
-                        "service" => "simpletemplate::add",
-                        "output" => 0,
+                        'message' => 'Unable to obtain associative sub domain for '. $self->{'domain'},
+                        'level' => 'warn',
+                        'output' => 0,
+                        'service' => 'simpletemplate::add',
+                        'backtraace' => 0,
                     }
                 );
             }
         }
-        
-        my $template = Template->new(
-            INCLUDE_PATH => $self->{'config'}->{'template_engine'}->{'template_directory'}.'/'. $self->{'config'}->{'template_engine'}->{'service_type'},
-            OUTPUT_PATH => $self->{'config'}->{'service_config'}->{'service_config_directory'},
-        );
-        
-        # Get file name
-        my $file_name = $self->{'domain'};
-        
-        if ( $self->{'config'}->{'service_config'}->{'config_style'} =~ m/split/i ){
-            # Write file
-            $template->process( 'simple_template.tpl', \%vhost, $self->{'domain'} ) || print $template->error();
-        }
         else{
-            # monolithic not implemented yet
+            Cpanel::Logger::logger(
+                {
+                    "message" => "Unable to load SimpleTemplate::AddonDomain: $@",
+                    "level" => "warn",
+                    "backtrace" => 0,
+                    "service" => "simpletemplate::add",
+                    "output" => 0,
+                }
+            );
         }
-        $self->_post_update_processing( 'add' );
-        return 1;
     }
     else{
-        Cpanel::Logger::logger(
-            {
-                'message' => 'Failed to load userdata to add the virtual host config',
-                'level' => 'warn',
-                'output' => 0,
-                'service' => 'simpletemplate::add',
-            }
-        );
+        if( $self->_load_userdata() ){
+            $vhost{'vhost'} = $self->{'vhost'};
+        }
     }
+
+    my $template = Template->new(
+        INCLUDE_PATH => $self->{'config'}->{'template_engine'}->{'template_directory'}.'/'. $self->{'config'}->{'template_engine'}->{'service_type'},
+        OUTPUT_PATH => $self->{'config'}->{'service_config'}->{'service_config_directory'},
+    );
+
+    # Get file name
+    my $file_name = $self->{'domain'};
+
+    if ( $self->{'config'}->{'service_config'}->{'config_style'} =~ m/split/i ){
+        # Write file
+        $template->process( 'simple_template.tpl', \%vhost, $self->{'domain'} ) || print $template->error();
+    }
+    else{
+        # monolithic not implemented yet
+    }
+    $self->_post_update_processing( 'add' );
+    return 1;
 }
 
 1;
